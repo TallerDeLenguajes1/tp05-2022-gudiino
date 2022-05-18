9aab6b69700a623b3183342931ab9156c186280f
gudiino <gudino1387@gmail.com>
2022-05-18 11:02:41 -0300

    Ej 3 calculadora V2

--- Program.cs
+++ Program.cs
@@ -11,3 +11,20 @@ while(num!=0){
 }
 Console.WriteLine();
 Console.WriteLine("-----FIN-----");
+//Creacion de la calculadora
+Console.WriteLine("CALCULADORA 2.0");
+Console.WriteLine("Ingrese un números");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Operaciones sobre el numero");
+Console.WriteLine("Valor Absoluto = " + Math.Abs(x));
+Console.WriteLine("Cuadrado = " + Math.Pow(x,2));
+Console.WriteLine("Raiz Cuadrada = " + Math.Sqrt(x));
+Console.WriteLine("Seno = " + Math.Sin(x));
+Console.WriteLine("Coseno = " + Math.Cos(x));
+Console.WriteLine("Parte entera = " + Math.Round(x));
+Console.WriteLine("Ingrese dos números");
+double z = Convert.ToDouble(Console.ReadLine());
+double y = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("El número mayor es: " + Math.Max(z,y));
+Console.WriteLine("El número menor es: " + Math.Min(z,y));
+Console.WriteLine("-----FIN-----");
