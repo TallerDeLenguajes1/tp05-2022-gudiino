5c3790588f064e38340ba4c43ef89191d777c76b
gudiino <gudino1387@gmail.com>
2022-05-17 21:37:03 -0300

    Desarrollo del ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("ingreso prueba uno");
-Console.WriteLine("Hello, World 2!");
+﻿using System;
+Console.WriteLine("Hola mundo");
+Console.WriteLine("Invertiendo un numero");
+Console.WriteLine("Ingrese un numero entero para ser invertido");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("El número ingresado es: " + num);
+Console.Write("Su número invertido es: ");
+while(num!=0){
+    Console.Write(num%10);
+    num=num/10;
+}
+Console.WriteLine();
+Console.WriteLine("-----FIN-----");
